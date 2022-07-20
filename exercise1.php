157693a9e77cc65748a3800a516ce04d4517cb94
mal031100 <88300591+mal031100@users.noreply.github.com>
2022-07-20 17:07:12 +0700

    php basic

--- exercise1.php
+++ exercise1.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
-</head>
-<body>
-     <?php
+<?php
           echo "<pre>file 1<pre>";
           $file1 = "1.txt";
           $file = fopen($file1,"r");
@@ -33,6 +24,8 @@
           }else{
                echo "Chuỗi không hợp lệ";
           }
+          echo "aaaa";
+          echo 'aaaa';
      ?>
-</body>
-</html>
+     <!-- echo ""
+     echo '' -->
